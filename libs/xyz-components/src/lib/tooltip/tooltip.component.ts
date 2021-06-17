3b6bcbda7e01387555bb11a20c7be63610af973d
eag1ex <eaglex@eaglex.net>
2021-06-17 15:49:12 +0000

    update toolpit current selection

--- libs/xyz-components/src/lib/tooltip/tooltip.component.ts
+++ libs/xyz-components/src/lib/tooltip/tooltip.component.ts
@@ -5,30 +5,41 @@ import { tooltipList } from '@xyz/data'
 import { log } from 'x-utils-es'
 import { Router } from '@angular/router';
 
+interface XTooltipItem extends ITooltipItem {
+  selected?: boolean;
+}
+
 @Component({
     selector: 'lib-tooltip',
     templateUrl: './tooltip.component.html',
     styleUrls: ['./tooltip.component.scss'],
 })
 export class TooltipComponent implements OnInit {
-    public tooltipList = tooltipList
+    public tooltipList: XTooltipItem[] = tooltipList
 
     chipList = new FormGroup({
         chip: new FormControl({})
     })
 
-    constructor(private router: Router) {}
+    constructor(private router: Router) {
+      // preset new toopit se selected
+      this.tooltipList.forEach(n => {
+        if (n.name === 'top')  n.selected = true
+
+      })
+    }
 
     get chip() {
         return this.chipList.get('chip')
     }
 
-    public chipClicked(val: ITooltipItem): void {
+    public chipClicked(val: XTooltipItem): void {
       this.chipList.patchValue({chip: val})
     }
 
     ngOnInit(): void {
-        this.chipList.get('chip').valueChanges.subscribe((n: ITooltipItem) => {
+        this.chipList.get('chip').valueChanges.subscribe((n: XTooltipItem) => {
+          n.selected = true
           this.router.navigate([`app/stories/${n.name}`]).then(nn => {
            // if (nn) log('navigated to', n)
           })
