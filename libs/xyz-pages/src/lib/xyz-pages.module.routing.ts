da5a334b5c047463acba6255900a359ce99cce24
eag1ex <eaglex@eaglex.net>
2024-01-17 13:46:12 +0700

    update node version

--- libs/xyz-pages/src/lib/xyz-pages.module.routing.ts
+++ libs/xyz-pages/src/lib/xyz-pages.module.routing.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 const routes: Routes = [
   {
     path: 'stories/:story', // top, best, new
